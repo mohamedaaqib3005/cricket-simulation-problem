6447374ecb064ced3ea06010f29b3e13d2155406
Aaqib <drop.mohamedaaqib@gmail.com>
2025-12-08 13:16:15 +0530

    write optimised psuedocode

--- solution.js
+++ solution.js
@@ -10,14 +10,17 @@
 // 8.If the balls reduce by six change the bowler and change the strike of batsman.
 // 9.Give running this random results for 24 balls or until there are 4 wicket are taken or 40 runs are scored whichever is first.
 // 10.Return the result of the match.
+
 /**
  * Returns the  outcome for each ball
  * @param {number[]} probablities
- * @param {string[]} outcomes
- * @returns {string} outcome
+ * @returns {string|number} outcome
  */
-function randomOutcome(probablities, outcomes) {
-  return outcome;
+function simulateBall(batsman) {
+  const outcomes = [0, 1, 2, 3, 4, 5, 6, "W"]
+
+
+  // based on the probablity return the outcome
 }
 /**
  * Returns the match summary
@@ -28,3 +31,46 @@ function simulateScore() {
   return matchSummary;
 
 }
+
+/**
+ * Returns the  outcome for each ball
+ * @param {number[]} probablities
+ * @returns {string|number} outcome
+ */
+function simulateBall(batterName) {
+  // looks up batter in probabilities chart
+  // randomly determines outcome
+  // return outcome as str | num
+}
+
+
+/**
+ * Returns updatedGamestate
+ * @param {obhect} gameState
+ * @returns {object} updatedGamestate
+ */
+function simulateOver(gameState) {
+  // while loop with the gamestate of balls
+  // call simulateBall with strike batsman
+  // based on the outcome update gameState
+  // break if the target is acheived or the wickets are over
+  // return updatedGamestate
+
+}
+
+
+/**
+ * Returns the  match summary
+ *
+ * @returns {string|number} matchSummary
+ */
+function simulateMatch() {
+  //create the initial gamestate
+  // while until match end (target,over,wickets)
+  // call simulateOver function
+  // change batsman after each over
+  // print the final match state
+
+
+}
+
